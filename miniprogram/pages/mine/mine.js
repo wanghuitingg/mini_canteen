b2feebd6b36158bb019004e1863c8ad4dd442640
wanghuitingg <1687752378@qq.com>
2022-04-07 16:30:10 +0800

    407

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -1,13 +1,68 @@
 // pages/mine/mine.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    username: "",
+    openid: '',
   },
-
+  // 获取用户信息
+  getUserInfo(e) {
+    console.log(e);
+    this.setData({
+      username: e.detail.userInfo.nickName
+    })
+  },
+  getopenid() {
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'open',
+      success: (res) => {
+        var usid = res.result.openid
+        console.log(usid)
+        this.setData({
+          openid: res.result.openid,
+        })
+        getApp().globalData.userid = res.result.openid
+      },
+      fail(res) {
+        console.log("获取失败", res);
+      }
+    })
+  },
+  // 跳转“个人资料”
+  getPersonalInfo:function(e){
+    wx.navigateTo({
+      url: '../personalInfo/personalInfo',
+    })
+  },
+  // 跳转“帮助中心”
+  help:function(e){
+    wx.navigateTo({
+      url: '../help/help',
+    })
+  },
+  // 跳转“意见反馈”
+  feedback:function(e){
+    wx.navigateTo({
+      url: '../feedback/feedback',
+    })
+  },
+    // 跳转页面“关于我们”
+    aboutUs:function(e) {
+      wx.navigateTo({
+        url: '../aboutUs/aboutUs',
+      })
+    },
+    // 跳转“商家管理”
+    management:function(e){
+      wx.navigateTo({
+        url: '../management/management',
+      })
+    },
   /**
    * 生命周期函数--监听页面加载
    */
